51d0af34e3494aa73bec7a413770eb6cc6b6c177
Raquel Patiño <r.patino.navarro@gmail.com>
2025-04-01 14:16:15 +0200

    test for email format done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'username'=> fake()->unique()->userName(),
+            'telephone'=> fake()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -2,19 +2,61 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+    
+    
+    public function test_email_has_correct_format(){
+        $user= User::factory()->create();
+        $userData= $user->toArray();
+        $response = $this->postJson('/api/register', $userData);
 
-        $response->assertStatus(200);
+        $response->assertStatus(201);
+    }
+    /**
+     * @dataProvider invalidEmails
+     */
+
+    public function test_email_fails_with_incorrect_format($email): void
+    {   
+        $user= User::factory()->make();
+        $userData= $user->toArray();
+        $userData ['email']= $email;
+        $response = $this->postJson('/api/register', $userData);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_email_is_not_unique_in_database(){
+
+        $user= User::factory()->create();
+        $userToTest= User::factory()->make();
+        $userData= $userToTest->toArray();
+        $userData ['email']= $user->email;
+        $response= $this->postJson('/api/register', $userData);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['email']);
+
+
+    }
+
+    public static function invalidEmails(){
+
+        return [
+            'no @'=> ['usuariohotmail.com'],
+            'no .com' => ['usuario@hotmail'],
+            'no domain'=> ['usuario@'],
+            'espaces'=> ['usuario@ hotmail. com'],
+            'empty'=> ['']
+
+        ];
     }
 }
