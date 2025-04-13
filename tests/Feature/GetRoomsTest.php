e37c8abb5f47fa34d3149e27cb4055cc6fb15e1c
Raquel Patiño <r.patino.navarro@gmail.com>
2025-04-13 13:39:21 +0200

    happy path for fetching rooms

---
+++ tests/Feature/GetRoomsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Laravel\Passport\Passport;
+use Database\Seeders\RoomSeeder;
+use Database\Seeders\HotelSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GetRoomsTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_user_can_get_rooms_by_hotel_id()
+    {
+        $this->seed(HotelSeeder::class);
+        $this->seed(RoomSeeder::class);
+        $user= User::factory()->create();
+        Passport::actingAs($user, [], 'api');
+
+        $response = $this->getJson('/api/hotels/1/rooms');
+
+        $response->assertStatus(200);
+        $response->dump();
+    }
+}
